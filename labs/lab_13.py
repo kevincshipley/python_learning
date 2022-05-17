cd079fafc3b142208303f626529b638a261cf174
kevin8pizza <apeesque@gmail.com>
2022-05-16 19:35:34 -0700

    Project 4 done, Lab 13 done

---
+++ labs/lab_13.py
@@ -0,0 +1,11 @@
+# Define an Employee class.
+class Employee:
+
+    # Include a constructor that creates instance variables name, hours worked,
+    # and hourly rate.
+
+    # The constructor should be able to be called with 0 to 3 arguments.
+
+    # HINT: def __init__(self, name="", hours=0, rate=0):
+
+    def __init__(self, name="", hours=0, rate=0):
